167540b021d3c9804d2dde1fa22b46cdb5c182af
Elliot Choules <elliot.choules@gmail.com>
2024-10-30 21:02:55 +0000

    Install Entity Framework NuGet packages
    Installed Microsoft.EntityFrameworkCore, Microsoft.EntityFrameworkCore.SqlServer, Microsoft.EntityFrameworkCore.Tools

---
+++ Dino.Api/Models/Dinosaur.cs
@@ -0,0 +1,11 @@
+﻿namespace Dino.Api.Models
+{
+    public class Dinosaur
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Classification { get; set; }
+        public string? Diet { get; set; }
+        public string? Period { get; set; }
+    }
+}
